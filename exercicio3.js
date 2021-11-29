591314bd3e8c51cbad4296fe86417305ef9f2fcd
Giulio Lelis <giuliolsc1405@gmail.com>
2021-11-28 23:24:52 -0300

    Exercicio 3

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,10 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+const botoesExpandirRetrairEl = document.querySelectorAll('.botao-expandir-retrair');
+
+botoesExpandirRetrairEl.forEach((el) => {
+    el.addEventListener('click', function (e) {
+        const paragrafoPai = e.currentTarget.parentNode;
+        paragrafoPai.classList.toggle('expandido');
+    });
+})
